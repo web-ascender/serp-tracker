e08411956e2dae87f76a24006fba730d250c8a7c
Ryan Doom <ryan.doom@webascender.com>
2012-12-14 09:53:29 -0500

    first commit of generated rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SerpTracker::Application.config.secret_token = '7662ede2f6a02cf03147d6821a9f2ae4a9bc8c5e62dddbb525677f06c8b2a0ffaad7eceb75fdd6ecc7bfa971bf26b36eb02db0a6be6036f98f16fdf3b1e1c1a2'
